33e7593a420763904361859a2a7cac9cea45fe5d
Nack <nack@buzzwoo.de>
2022-08-04 09:54:13 +0700

    Fix the json error

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -3,9 +3,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  let orders = {};
-  let order = {};
   if (req.method === "GET") {
+    let orders;
     try {
       orders = await prisma.order.findMany({
         include: {
@@ -20,6 +19,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   if (req.method === "POST") {
+    let order;
     try {
       order = await prisma.order.create({
         data: req.body,
